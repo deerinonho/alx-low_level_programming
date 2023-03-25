50af63519d48c825735f0532874ea9e587a478b8
deerinonho <achrafbouftouh@gmail.com>
2023-03-25 12:38:19 -0700

    rest of codes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 25; i >= 0; i--)
+{
+putchar(i + 'a');
+}
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 16; i++)
+{
+putchar(i < 10 ? i + '0' : i - 10 + 'a');
+}
+putchar('\n');
+return (0);
+}
